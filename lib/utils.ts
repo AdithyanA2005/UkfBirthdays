06d94bf3b5a2869c0f6ce90458c365928d2624cd
Adithyan A <deepaadithyan56@gmail.com>
2024-12-03 02:59:28 +0530

    Add reusable utiltily functiono

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function currentAge(birthday: Date) {
+  const birthDate = new Date(birthday);
+  const today = new Date();
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+
+  const hasBirthdayPassed =
+    today.getMonth() > birthDate.getMonth() ||
+    (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+
+  // Subtract one, if the birthday hasn't passed yet this year
+  return hasBirthdayPassed ? age : --age;
+}
+
+export function isSameDayAndMonth(date1: Date, date2: Date) {
+  return date1.getDate() === date2.getDate() && date1.getMonth() === date2.getMonth();
+}
