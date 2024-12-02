9a011673a462f27f990b4be358c5a3a1ba9903b2
Adithyan A <deepaadithyan56@gmail.com>
2024-12-03 04:32:46 +0530

    Hide view all birthdays button from user

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,9 +10,9 @@ export function Navbar() {
         </div>
 
         <div className="ml-auto">
-          <Link href="/all" className="rounded-md bg-primary px-4 py-2 text-primary-foreground hover:bg-primary/90">
+          {/* <Link href="/all" className="rounded-md bg-primary px-4 py-2 text-primary-foreground hover:bg-primary/90">
             View All Birthdays
-          </Link>
+          </Link> */}
         </div>
       </div>
     </nav>
