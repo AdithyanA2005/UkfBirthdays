6f53c681194b46437bd55b020005f44bec211594
Adithyan A <deepaadithyan56@gmail.com>
2024-12-09 23:21:03 +0530

    Style fix

--- components/birthday-card.tsx
+++ components/birthday-card.tsx
@@ -19,7 +19,7 @@ export function BirthdayCard({ name, age, date, semester, department, isBirthday
     <Card className="relative border-none bg-white text-center shadow-md transition-shadow hover:shadow-lg">
       <CardHeader className="gap-2.5 pb-4">
         {department || semester ? (
-          <div className="absolute right-0 top-0 rounded-bl-md rounded-tr-md bg-primary-600 px-4 py-0.5 text-sm font-extrabold text-white">
+          <div className="absolute right-0 top-0 rounded-bl-2xl rounded-tr-2xl bg-primary-600 px-3 py-1 text-sm font-extrabold text-white">
             {semester ? `S${semesterMappings[semester]} ` : null}
             {department ? department : null}
           </div>
@@ -27,21 +27,21 @@ export function BirthdayCard({ name, age, date, semester, department, isBirthday
 
         <Avatar className="mx-auto size-20 rounded-full">
           <AvatarImage src="" alt={name} />
-          <AvatarFallback className="bg-primary-100 text-[1.6rem] font-extrabold text-primary-600">
+          <AvatarFallback className="bg-primary-100 text-[1.6rem] font-extrabold text-primary-500">
             {name.slice(0, 2)}
           </AvatarFallback>
         </Avatar>
 
-        <CardTitle className="text-[1.35rem] font-bold text-primary-800">{name}</CardTitle>
+        <CardTitle className="text-[1.35rem] font-bold text-primary-600">{name}</CardTitle>
       </CardHeader>
 
       <CardContent className="grid grid-cols-2 gap-2 divide-x divide-primary-200 px-6 pb-6">
         <div className="flex flex-col items-center">
-          <span className="text-xs font-semibold text-primary-700">{age.title}</span>
+          <span className="text-[0.8rem] font-semibold text-primary-600">{age.title}</span>
           <span className="text-[1.4rem] font-bold text-primary-500">{age.value}</span>
         </div>
         <div className="flex flex-col items-center">
-          <span className="text-xs font-semibold text-primary-700">{date.title}</span>
+          <span className="text-[0.8rem] font-semibold text-primary-600">{date.title}</span>
           <span className="text-[1.4rem] font-bold text-primary-500">{format(date.value, "MMM do")}</span>
         </div>
       </CardContent>
